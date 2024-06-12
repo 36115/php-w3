a817dde26c8a3f1fa65ba8a38445b03433642ead
36115 <138181602+36115@users.noreply.github.com>
2024-06-12 14:39:10 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>สรวิชญ์ 610-11</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <?php
+                $a = "3";
+                $b = "4";
+                $c = $a + $b;
+
+                $A = "20.5";
+                $B = "20.5";
+                $C = $A == $B;
+
+                echo "<h1>";
+                echo "1. ผลรวมของ \$a + \$b "." คือ ".$c."</br>";
+                echo "2. "."ผลลัพธ์ = ".$C;
+                echo "</h1>";
+            ?>  
+        </div>
+    </div>
+</body>
+</html>
